5d99e6d048bab14ca92a709b21942353f649b1bc
SHK <lemona31@uos.ac.kr>
2023-06-28 16:35:13 +0900

    settings, basketPieChart modified

---
+++ src/pages/dashBoard/dashBoard.js
@@ -0,0 +1,121 @@
+import style from './dashBoard.module.css';
+import * as React from 'react';
+import { ResponsivePie } from '@nivo/pie';
+
+export const BasketPieChart = () => {
+
+    const myData = [
+        {
+          "id": "Success",
+          "label": "Success",
+          "value": 786,
+          "color": "hsl(163, 70%, 50%)"
+        },
+        {
+          "id": "Fail",
+          "label": "Fail",
+          "value": 494,
+          "color": "hsl(328, 70%, 50%)"
+        },
+      ];
+
+    return (
+        <div style={{width: '800px', height: '500px', }}>
+        <h2 style={{textAlign:'center'}}>재료 추출 통계</h2>
+        <ResponsivePie
+        data={myData}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        activeOuterRadiusOffset={8}
+        //pastel1,2,set3
+        colors={{ scheme: 'set3' }}
+        borderWidth={1}
+        borderColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    0.2
+                ]
+            ]
+        }}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: 'color' }}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    2
+                ]
+            ]
+        }}
+        defs={[
+            {
+                id: 'dots',
+                type: 'patternDots',
+                background: 'inherit',
+                color: 'rgba(255, 255, 255, 0.3)',
+                size: 4,
+                padding: 1,
+                stagger: true
+            },
+            {
+                id: 'lines',
+                type: 'patternLines',
+                background: 'inherit',
+                color: 'rgba(255, 255, 255, 0.3)',
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10
+            }
+        ]}
+        fill={[
+            {
+                match: {
+                    id: 'test1'
+                },
+                id: 'dots'
+            },
+            {
+                match: {
+                    id: 'test2'
+                },
+                id: 'lines'
+            },
+        ]}
+        legends={[
+            {
+                anchor: 'bottom',
+                direction: 'row',
+                justify: false,
+                translateX: 0,
+                translateY: 56,
+                itemsSpacing: 0,
+                itemWidth: 100,
+                itemHeight: 18,
+                itemTextColor: '#999',
+                itemDirection: 'left-to-right',
+                itemOpacity: 1,
+                symbolSize: 18,
+                symbolShape: 'circle',
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemTextColor: '#000'
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
+        </div>
+    );
+}
+
