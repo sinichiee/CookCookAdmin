e0e5f192a6aef6d0a4a2a8c69666286d1c43d432
agji12 <a@b>
2023-06-29 16:36:04 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,25 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
-import ClientUserList from './pages/userList/userList';
-import {Pies} from './pages/gptCount/gptCount';
-import DashBoard from './pages/dashBoard/dashBoard';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+// routes
+import Router from './routes';
+// theme
+import ThemeProvider from './theme';
+// components
+import { StyledChart } from './components/chart';
+import ScrollToTop from './components/scroll-to-top';
 
-function Navi() {
+// ----------------------------------------------------------------------
+
+export default function App() {
   return (
-    <Nav variant="tabs" defaultActiveKey="/home">
-      <Nav.Item>
-        <Nav.Link href="/adminPage/main">메인</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/adminPage/userList" eventKey="link-2">회원관리</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/adminPage/basket" eventKey="link-2">GPT Chart</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/adminPage/shop" eventKey="link-3">공동구매</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/adminPage/board" eventKey="link-4">게시판</Nav.Link>
-      </Nav.Item>
-       <Nav.Item>
-       <Nav.Link href="/adminPage/UserList" eventKey="link-6">회원</Nav.Link>
-       </Nav.Item>
-    </Nav>
-
-  );
-}
-
-
-function App() {
-  return (
-    <div className="container">
+    <HelmetProvider>
       <BrowserRouter>
-        <Navi></Navi>
-        <Routes>
-          <Route path='/adminPage/main' element={<DashBoard/>}></Route>
-          <Route path='/adminPage/basket' element={<Pies/>}></Route>
-          <Route path="/adminPage/userList" element={<ClientUserList/>}/>
-        </Routes>
+        <ThemeProvider>
+          <ScrollToTop />
+          <StyledChart />
+          <Router />
+        </ThemeProvider>
       </BrowserRouter>
-    </div>
+    </HelmetProvider>
   );
 }
-
-export default App;
