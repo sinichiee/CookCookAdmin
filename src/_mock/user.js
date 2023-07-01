84061186c1c62dbb02b49e18e1d731018dd8b126
이가은 <101073648+dmsalsgml@users.noreply.github.com>
2023-07-01 18:45:30 +0900

    userList 기능 일부 수정, 쿼리문 못고침

--- src/_mock/user.js
+++ src/_mock/user.js
@@ -4,22 +4,38 @@ import { sample } from 'lodash';
 import { useState } from 'react';
 
 // ----------------------------------------------------------------------
-// const USERLIST = async () => {
-//   const [users, setUser] = useState();
-//   const response = await axios.get('/data/selectUserList').then((resp) => {
-//     console.log(resp);
-//   });
+const USERLIST = async () => {
+  const [users, setUser] = useState([]);
+  axios.get('/data/selectUserList')
+  .then((resp) => {
+    setUser(resp.data);  
+  });
   
-// };
+  const userData = [
+    {
+      code: users.code,
+      // avatarUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
+      id : users.id,
+      nickname: users.nickname,
+      company: users.companyname,
+      // 이건 delDate가 있냐없냐로 active, banned로 정해서 값지정하기
+      status : "",
+      auth:  users.auth,
+      reportCount: users.reportCount
+    }
+  ];
+};
 axios.get('/data/selectUserList').then((resp) => {
   console.log(resp);
 });
 const users = [...Array(24)].map((_, index) => ({
   id: faker.datatype.uuid(),
   avatarUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
-  name: faker.name.fullName(),
+  name: faker.company.name(),
+  nickname: faker.company.name(),
   company: faker.company.name(),
   status: sample(['active', 'banned']),
+  reportCount : sample([0,1,2,3,4,5,6,7,8,9,10]),
   role: sample([
     '일반',
     '사업자'
